9aeee3e5bb24d64927dabd5fe0272b73354fd710
Khiu Kim Hong <khkhiu@gmail.com>
2023-05-28 16:37:35 +0800

    Added script for square

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{   
+    public float speed = 100f;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    // Get called once for entire life cycle of script
+    private void Start()
+    {
+        AddStartingForce();
+    }
+
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? -1f : 1f;
+        // Add random element to movement
+        float y = Random.value < 0.5f ? Random.Range(-1f, -0.5f) : Random.Range(0.5f, 1f);
+        
+        Vector2 _direction = new Vector2(x, y);
+        _rigidbody.AddForce(_direction * 3 *this.speed);
+    }
+}
